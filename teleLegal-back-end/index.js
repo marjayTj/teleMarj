a13463a035fe9a7c8e9089c7c4a5702da71e9df1
Marjay <mjbumalod@gmail.com>
2024-10-27 20:33:18 +0800

    First Commit

---
+++ teleLegal-back-end/index.js
@@ -0,0 +1,4 @@
+//this is our entry point -run modemon here
+
+require('./socketServer');
+require('./expressRoutes')
---
+++ telelegal-front-end/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { createStore } from 'redux'; 
+import { Provider} from 'react-redux' 
+import rootReducer from './redux-elements/reducers/rootReducer';
+
+const theStore = createStore(rootReducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <Provider store={theStore}>
+    <App />
+  </Provider>
+);
